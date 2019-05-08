3ba07661b21a12f3b3320c34ec81e22064dfd1d0
Mohammed Danish Shaikh <danish778866@gmail.com>
2019-05-08 17:32:12 -0500

    Driver updates

--- src/driver.py
+++ src/driver.py
@@ -17,7 +17,8 @@ class Driver:
         self.lambda_config = self.config.get_section('Lambda')
         self.storage_class_dict = {"redis": "SerferRedisStorage", "memcache": "SerferMemcacheStorage"}
         self.redis_storage_class = getattr(storage, self.storage_class_dict[self.storage_config["type"]])
-        self.storage = self.redis_storage_class(self.storage_config["host"], self.storage_config["port"])
+        print(self.storage_config["mode"])
+        self.storage = self.redis_storage_class(self.storage_config["host"], self.storage_config["port"], self.storage_config["mode"])
         self.conn = self.storage.get_storage_handle()
         self.query = query
         self.id = id
@@ -26,6 +27,38 @@ class Driver:
         self.logfile = logfile
         self.file_handle = open(logfile, "w")
         self.barrier_num = 0
+        self.poll_time = 0
+        self.layer_times = []
+        self.store_pull_time = 0
+        self.store_push_time = 0
+        self.merge_time = 0
+        self.split_time = 0
+        self.current_layer = -1
+
+    def write_microbenchmarks(self):
+        self.file_handle.write("Poll Time: " + str(self.poll_time))
+        self.file_handle.write("Layer Times: " + str(self.layer_times))
+        self.file_handle.write("Store Push Time: " + str(self.store_push_time))
+        self.file_handle.write("Store Pull Time: " + str(self.store_pull_time))
+        self.file_handle.write("Merge Time: " + str(self.merge_time))
+        self.file_handle.write("Split Time: " + str(self.split_time))
+
+    def update_microbenchmarks(self, microbenchmark, time_taken):
+        if microbenchmark == "poll":
+            self.poll_time += time_taken
+        elif microbenchmark == "get":
+            self.store_pull_time += time_taken
+        elif microbenchmark == "set":
+            self.store_push_time += time_taken
+        elif microbenchmark == "layer":
+            if len(self.layer_times) == self.current_layer:
+                self.layer_times.append(0)
+            self.layer_times[self.current_layer] += time_taken
+        elif microbenchmark == "merge":
+            self.merge_time += time_taken
+        elif microbenchmark == "split":
+            self.split_time += time_taken
+
 
     def barrier(self, lambda_keys):
         self.storage.purge_persisted_values()
@@ -117,6 +150,7 @@ class Driver:
 
     def run(self):
         start_time = default_timer()
+        self.current_time = start_time
         img = self.query
         fn_names = self.lambda_config["fn_names"].split(",")
         driver_suffix = self.driver_config["suffix"]
@@ -128,9 +162,14 @@ class Driver:
         self.convert_to_int(overlap_sizes)
         self.storage.set_group_by(2)
         for idx, fn_name in enumerate(fn_names[0:2]):
+            self.current_layer += 1
+            micro_start_time = default_timer()
             s_imgs = self.split_image(img, split_input_sizes[idx])
+            micro_end_time = default_timer()
+            self.update_microbenchmarks("split", (micro_end_time - micro_start_time))
             k = 0
             lambda_keys = []
+            start_layer_time = default_timer()
             for i, split in enumerate(s_imgs):
                 for j, s in enumerate(split):
                     key_name = image_name + splits[k] +  str(idx) +  driver_suffix
@@ -138,35 +177,57 @@ class Driver:
                     lambda_key_name = lambda_key_name.split('.')[0]
                     k = k + 1
                     lambda_keys.append(lambda_key_name)
+                    micro_start_time = default_timer()
                     self.storage.write_to_store(key_name, s)
+                    micro_end_time = default_timer()
+                    self.update_microbenchmarks("set", (micro_end_time - micro_start_time))
                     payload="{\"key\": \"" + key_name + "\"}"
                     response = self.lambda_client.invoke(
                                 FunctionName=fn_name,
                                 InvocationType="Event",
                                 Payload=payload
                             )
+            micro_start_time = default_timer()
             while self.barrier(lambda_keys):
                 continue
+            micro_end_time = default_timer()
+            self.update_microbenchmarks("poll", (micro_end_time - micro_start_time))
+            end_layer_time = default_timer()
+            self.update_microbenchmarks("layer", (micro_end_time - micro_start_time))
             intermediate_values = self.storage.get_persisted_values()
+            micro_start_time = default_timer()
             img = self.merge_imgs(intermediate_values, overlap_sizes[idx])
+            micro_end_time = default_timer()
+            self.update_microbenchmarks("merge", (micro_end_time - micro_start_time))
+        self.current_layer += 1
         key_name = image_name +  "3" +  driver_suffix
         lambda_key_name = re.sub('[0-9]+', self.my_replace, key_name)
         lambda_key_name = lambda_key_name.split('.')[0]
+        micro_start_time = default_timer()
         self.storage.write_to_store(key_name, img)
+        micro_end_time = default_timer()
+        self.update_microbenchmarks("set", (micro_end_time - micro_start_time))
         payload="{\"key\": \"" + key_name + "\"}"
         fn_name = "alex2"
+        start_layer_time = default_timer()
         response = self.lambda_client.invoke(
                    FunctionName=fn_name,
                    InvocationType="Event",
                    Payload=payload
                )
         self.storage.set_group_by(1)
+        micro_start_time = default_timer()
         while self.barrier([lambda_key_name]):
             continue
+        micro_end_time = default_timer()
+        self.update_microbenchmarks("poll", (micro_end_time - micro_start_time))
+        end_layer_time = default_timer()
+        self.update_microbenchmarks("layer", (micro_end_time - micro_start_time))
         f_out = self.storage.get_persisted_values()[0][0]
         self.file_handle.write(str(f_out.shape))
         self.file_handle.write("\n")
         duration = default_timer() - start_time
+        self.write_microbenchmarks()
         self.file_handle.write("Time: " + str(duration))
         self.file_handle.write("\n")
         self.file_handle.close()
